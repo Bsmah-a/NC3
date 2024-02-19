ba1bb008d209cfe9d7a9636580880e9adfe25c95
Bsmah <ftooo_19@hotmail.com>
2024-02-19 14:27:16 +0300

    fluet

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,13 @@
 import Vapor
-//import Fluent
-//import FluentPostgresDriver
+//import PostgresKit
+
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+   /* app.databases.use(.postgres(configuration: SQLPostgresConfiguration(hostname: "localhost", username: "postgres", password: "", database: "best_isp", tls: .prefer(try .init(configuration: .clientDefault)))),     as: .psql)*/
     // register routes
     try routes(app)
 }
